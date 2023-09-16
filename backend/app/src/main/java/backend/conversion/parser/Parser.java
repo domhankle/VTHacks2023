1c0f37051371f15d124b1f78a1e464e4ae919aed
Victor <vlondon2@gmu.edu>
2023-09-16 13:15:22 -0400

    Editing my branch.

--- backend/app/src/main/java/backend/conversion/parser/Parser.java
+++ backend/app/src/main/java/backend/conversion/parser/Parser.java
@@ -2,5 +2,30 @@ package main.java.backend.conversion.parser;
 
 
 public class Parser {
-    
+    public ArrayList<String> parse(String path) {
+        try {
+
+        }
+
+
+
+
+    }
+
+    public static String readFileToString(String filePath) {
+        try {
+            // Convert the file path string to a Path object
+            Path path = Paths.get(filePath);
+
+            // Use Files.readAllBytes to read the file content into a byte array
+            byte[] fileBytes = Files.readAllBytes(path);
+
+            // Convert the byte array to a String using UTF-8 encoding (adjust as needed)
+            return new String(fileBytes, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+
+        }
+    }
+
+    private
 }
