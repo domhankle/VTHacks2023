6d8489d989761e5326dfb70d16eb993d8c1fb4c3
Victor <vlondon2@gmu.edu>
2023-09-16 19:22:51 -0400

    Updated

---
+++ backend/app/src/main/java/backend/controller/Controller.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "please kill yourself";
+    }
+}
