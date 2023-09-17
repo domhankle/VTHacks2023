d4704621e654bb504c91e6c271a50c8138ae1f44
Victor <vlondon2@gmu.edu>
2023-09-17 03:29:48 -0400

    ._.

--- ui/java-source-parser/src/app/header-bar/header-bar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header-bar',
-  templateUrl: './header-bar.component.html',
-  styleUrls: ['./header-bar.component.css'],
-})
-export class HeaderBarComponent {}
